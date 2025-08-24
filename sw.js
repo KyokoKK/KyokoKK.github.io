592dc5bfb9ef7df59bf899de013aea0202ebb751
KyokoKK <228305790+KyokoKK@users.noreply.github.com>
2025-08-24 19:50:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
-self.addEventListener('install', (e) => self.skipWaiting());
+// Minimal SW for installability + basic cache
+const CACHE = 'diet-mvp-v0';
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png']))
+  );
+  self.skipWaiting();
+});
 self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', () => { /* no-op */ });
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET' || new URL(e.request.url).origin !== self.location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
